3d325e1206ebcce3372cfb90e5dedd45f5feb1ad
cjh <949661474@qq.com>
2025-01-06 11:00:22 +0800

    首页-reload

--- src/components/Slider/Slider.vue
+++ src/components/Slider/Slider.vue
@@ -5,30 +5,30 @@
       mode="vertical"
       class="menu"
   >
-    <a-menu-item key="1">
+    <a-menu-item key="1" class="nav-text">
       <router-link to="/">
-        <span class="nav-text"><CrownTwoTone /> 开始</span>
+        <span><CrownTwoTone /> 开始</span>
       </router-link>
     </a-menu-item>
-    <a-menu-item key="2">
-      <span class="nav-text iconfont icon-qita">前端</span>
+    <a-menu-item key="2" class="nav-text">
+      <span>前端</span>
     </a-menu-item>
-    <a-menu-item key="3">
-      <span class="nav-text">后端</span>
+    <a-menu-item key="3" class="nav-text">
+      <span >后端</span>
     </a-menu-item>
-    <a-menu-item key="4">
+    <a-menu-item key="4" class="nav-text">
       <router-link to="/home">
-        <span class="nav-text">工具</span>
+        <span>工具</span>
       </router-link>
     </a-menu-item>
-    <a-menu-item key="5">
-      <span class="nav-text">日志</span>
+    <a-menu-item key="5" class="nav-text">
+      <span>日志</span>
     </a-menu-item>
-    <a-menu-item key="6">
-      <span class="nav-text">留言板</span>
+    <a-menu-item key="6" class="nav-text">
+      <span>留言板</span>
     </a-menu-item>
-    <a-menu-item key="7">
-      <span class="nav-text">其他</span>
+    <a-menu-item key="7" class="nav-text">
+      <span>其他</span>
     </a-menu-item>
   </a-menu>
 </template>
@@ -42,32 +42,22 @@ const selectedKeys = ref<string[]>(['1']);
 </script>
 
 <style scoped>
-
-
 .menu {
-  flex-grow: 1; /* 使菜单填满侧边栏 */
   display: flex;
   flex-direction: column;
-  justify-content: space-around; /* 垂直居中 */
-  height: 50vh;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  list-style: none;
 }
 
 .nav-text {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  text-decoration: none;
-  color: var(--text-secondary);
-  font-size: 1.0rem;
-  transition: color 0.3s, background-color 0.3s;
-  white-space: nowrap;
-  padding: 0.5rem 1rem;
-  border-radius: 6px;
-}
-
-.nav-text:hover {
-  color: #1890ff;
-  background-color: rgba(24, 144, 255, 0.08);
+  font-size: 16px;
+  text-align: center;
+  padding: 20px 15px;
+  display: inline-block;
+  transition: transform 0.3s ease; /* 平滑过渡效果 */
 }
 
 </style>
